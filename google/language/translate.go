a932444a23f2c93cc0eacdd9ad26dbf11eb50e25
unknown <mattn.jp@gmail.com>
2010-07-30 16:00:01 +0900

    remove debug message.

--- google/language/translate.go
+++ google/language/translate.go
@@ -7,7 +7,6 @@ import "io/ioutil"
 import "bytes"
 
 func Translate(from, to, s string) (t string, err os.Error) {
-	println("http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=" + http.URLEscape(from) + "&langpair=" + "%7C" + http.URLEscape(to))
 	if r, _, err := http.Get("http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=" + http.URLEscape(s) + "&langpair=" + http.URLEscape(from) + "%7C" + http.URLEscape(to)); err == nil {
 		defer r.Body.Close()
 		if b, err := ioutil.ReadAll(r.Body); err == nil {
